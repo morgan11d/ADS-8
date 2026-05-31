1d1edf2575d0efc559eb4a644dabe06b3b05efcf
morgan11d <morkicel@gmail.com>
2026-05-31 10:44:31 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
-// Copyright 2021 NNTU-CS
-#include "bst.h"
+#include "../include/bst.h"
+#include "alg.cpp"
 
 int main() {
+    BST<std::string> tree;
+    makeTree(tree, "src/war_peace.txt");
+    printFreq(tree);
     return 0;
 }
